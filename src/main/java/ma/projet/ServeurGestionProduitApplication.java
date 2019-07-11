71801ec95e97a18f1a8bb5f4736a5c056ce1bd30
MohssineElAttari <14mohssine@gmail.com>
2019-07-11 12:33:57 +0100

    first commit

---
+++ src/main/java/ma/projet/ServeurGestionProduitApplication.java
@@ -0,0 +1,13 @@
+package ma.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionProduitApplication.class, args);
+	}
+
+}
